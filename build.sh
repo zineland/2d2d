223923cbfbe23d81e3e3f948a2b3dae026e473a2
Folyd <lyshuhow@gmail.com>
2022-03-22 21:13:55 +0800

    Try fix build script

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ set -e
 if ! command -v zine &> /dev/null
 then
     echo "zine could not be found, start installing..."
-    cargo install zine
+
+    RUSTUP_TOOLCHAIN=stable cargo install zine
 fi
 
 zine build
