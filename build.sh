de1b63822ce458a623ad92a7b9ca738f4def8943
Folyd <lyshuhow@gmail.com>
2022-04-05 11:58:30 +0800

    Auto build and deploy via CI

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+BRANCH="gh-pages"
+
 if ! command -v zine &> /dev/null
 then
     echo "zine could not be found, start installing..."
@@ -9,4 +11,25 @@ then
     RUSTUP_TOOLCHAIN=stable cargo install zine
 fi
 
-zine build
+TEMP_DIR="/tmp/2d2d-build"
+
+zine build ${TEMP_DIR}
+
+deploy
+
+deploy() {
+  echo "Starting deploying..."
+  git config --global url."https://".insteadOf git://
+  git config --global url."https://github.com/".insteadOf git@github.com:
+
+  git checkout ${BRANCH}
+  cp -vr ${TEMP_DIR} .
+  git config user.name "GitHub Actions"
+  git config user.email "github-actions-bot@users.noreply.github.com"
+  git add .
+  git commit -m "Zine build"
+  git push "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" ${BRANCH}
+
+  echo "Deploy complete"
+}
+
